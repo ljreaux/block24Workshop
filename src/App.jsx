491b05eab103c32bf4c360a547868e213cb51863
Larry John Reaux <larryreaux@Larrys-Air.attlocal.net>
2023-12-19 19:30:32 -0600

    finished Puppy Data with useState tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-
+import { puppyList } from "./data";
+import { useState } from "react";
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
 
   return (
-    <>
-      
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
